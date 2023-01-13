52f929d7fb000f83dba78d66d299a8e0c59becc9
Nico Verbruggen <mail@nicoverbruggen.be>
2023-01-13 23:35:04 +0100

    First experiment

--- bulk-add-cert/main.swift
+++ bulk-add-cert/main.swift
@@ -6,6 +6,56 @@
 //
 
 import Foundation
+import Security
+import Cocoa
 
-print("Hello, World!")
+print("We will attempt to add some certificate(s)... let's go!")
+
+// we will need to convert .key to .der
+// e.g.
+/**
+ We will need to convert the .key to .der format:
+
+ ```sh
+ openssl x509 -in /Users/nicoverbruggen/.config/valet/CA/LaravelValetCASelfSigned.pem -out /Users/nicoverbruggen/.config/valet/CA/LaravelValetCASelfSigned.der -outform DER
+
+ openssl x509 -in /Users/nicoverbruggen/.config/valet/Certificates/nicoverbruggen.test.crt -out /Users/nicoverbruggen/.config/valet/Certificates/nicoverbruggen.test.der -outform DER
+
+ As per: https://developer.apple.com/forums/thread/68789
+ ```
+ */
+
+func addCertificateWithPath(path: String) {
+    let url = URL(fileURLWithPath: path)
+    guard let data = NSData(contentsOf: url) else {
+        print("certificate not found")
+        exit(1)
+    }
+
+    let certificate = SecCertificateCreateWithData(nil, data)
+
+    let addQuery: [String: Any] = [kSecClass as String: kSecClassCertificate,
+                                   kSecValueRef as String: certificate!]
+
+    let status = SecItemAdd(addQuery as CFDictionary, nil)
+
+    guard status == errSecSuccess else {
+        print("Could not add the certificate. Maybe it was added before?")
+        exit(1)
+    }
+
+    let outcome = SecTrustSettingsSetTrustSettings(certificate!, .admin, nil)
+
+    guard outcome == errSecSuccess else {
+        print("Could not add to admin trust.")
+        print(SecCopyErrorMessageString(outcome, nil))
+        exit(1)
+    }
+
+    print("Certificate for path \(path) has been added!")
+}
+
+// addCertificateWithPath(path: "/Users/nicoverbruggen/.config/valet/CA/LaravelValetCASelfSigned.der")
+addCertificateWithPath(path: "/Users/nicoverbruggen/.config/valet/Certificates/nicoverbruggen.test.der")
+
 
